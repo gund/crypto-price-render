78bdce4c72d941d8983e1a3ef9dbf59f077d1055
Alex Malkevich <malkevich.alex@gmail.com>
2022-06-01 12:38:54 +0200

    initial commit

---
+++ apps/crypto-price-render/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'crypto-price-render',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/crypto-price-render',
+};
