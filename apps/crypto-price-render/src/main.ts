d96fad1ef524e56e6cc22e6a8472cf7a8324525c
Alex Malkevich <malkevich.alex@gmail.com>
2022-11-13 18:36:11 +0100

    fix(docs): configure openapi for serverless

--- apps/crypto-price-render/src/main.ts
+++ apps/crypto-price-render/src/main.ts
@@ -1,24 +1,17 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import type { Handler } from 'aws-lambda';
 import { AppModule } from './app/app.module';
 
-const globalPrefix = 'api';
+const ApiPrefix = 'api';
+const DocsPrefix = 'docs';
 
 export async function bootstrapApp() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix(ApiPrefix);
 
-  return app;
-}
-
-async function bootstrapServer() {
-  const { DocumentBuilder, SwaggerModule } = await import('@nestjs/swagger');
-
-  const app = await bootstrapApp();
-
-  const port = process.env.PORT || 3333;
   const config = new DocumentBuilder()
     .setTitle('Crypto Price Render')
     .setDescription('Render current crypto price as HTML or JSON')
@@ -27,15 +20,23 @@ async function bootstrapServer() {
     .build();
 
   SwaggerModule.setup(
-    globalPrefix,
+    DocsPrefix,
     app,
     SwaggerModule.createDocument(app, config)
   );
 
+  return app;
+}
+
+async function bootstrapServer() {
+  const app = await bootstrapApp();
+
+  const port = process.env.PORT || 3333;
+
   await app.listen(port);
 
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${ApiPrefix}`
   );
 }
 
@@ -53,7 +54,20 @@ async function bootstrapLambda() {
 let server: Handler;
 
 export const handler: Handler = async (event, context, callback) => {
-  console.log('ENV', process.env);
+  if (event.path === `/${ApiPrefix}`) {
+    event.path = `/${ApiPrefix}/`;
+  } else if (event.path === `/${DocsPrefix}`) {
+    event.path = `/${DocsPrefix}/`;
+  }
+
+  event.path =
+    event.path.includes('swagger-ui') &&
+    !event.path.startsWith(`/${DocsPrefix}`)
+      ? `/${DocsPrefix}${event.path}`
+      : event.path;
+
+  console.log('PATH', event.path);
+
   server = server ?? (await bootstrapLambda());
   return server(event, context, callback);
 };
